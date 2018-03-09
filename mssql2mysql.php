61fc53481547d1087f5d40ae83ef469c6143090b
Vadzim Yatskevich <vadzimyatskevich@gmail.com>
2018-03-09 22:48:29 +0300

    Update mssql2mysql.php

--- mssql2mysql.php
+++ mssql2mysql.php
@@ -6,7 +6,6 @@ define('MSSQL_HOST','mssql_host');
 define('MSSQL_USER','mssql_user');
 define('MSSQL_PASSWORD','mssql_password');
 define('MSSQL_DATABASE','mssql_database');
-
 /*
  * DESTINATION: MySQL
  */
@@ -14,57 +13,35 @@ define('MYSQL_HOST', 'mysql_host');
 define('MYSQL_USER', 'mysql_user');
 define('MYSQL_PASSWORD','mysql_password');
 define('MYSQL_DATABASE','mysql_database');
-
-/*
- * SOME HELPER CONSTANT
- */
-define('CHUNK_SIZE', 1000);
-
 /*
  * STOP EDITING!
  */
-
 set_time_limit(0);
-
 function addQuote($string)
 {
 	return "'".$string."'";
 }
-
 function addTilde($string)
 {
 	return "`".$string."`";
 }
-
 // Connect MS SQL
-$mssql_connect = mssql_connect(MSSQL_HOST, MSSQL_USER, MSSQL_PASSWORD) or die("Couldn't connect to SQL Server on '".MSSQL_HOST."'' user '".MSSQL_USER."'\n");
+$mssql_db = sqlsrv_connect(MSSQL_HOST, ['Uid' => MSSQL_USER, 'PWD' => MSSQL_PASSWORD, 'Database' => MSSQL_DATABASE, 'CharacterSet' => 'UTF-8']) or die("Couldn't connect to SQL Server on '".MSSQL_HOST."'' user '".MSSQL_USER."'\n");
 echo "=> Connected to Source MS SQL Server on '".MSSQL_HOST."'\n";
-
-// Select MS SQL Database
-$mssql_db = mssql_select_db(MSSQL_DATABASE, $mssql_connect) or die("Couldn't open database '".MSSQL_DATABASE."'\n"); 
-echo "=> Found database '".MSSQL_DATABASE."'\n";
-
 // Connect to MySQL
-$mysql_connect = mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD) or die("Couldn't connect to MySQL on '".MYSQL_HOST."'' user '".MYSQL_USER."'\n");
-echo "\n=> Connected to Source MySQL Server on ".MYSQL_HOST."\n";
-
-// Select MySQL Database
-$mssql_db = mysql_select_db(MYSQL_DATABASE, $mysql_connect) or die("Couldn't open database '".MYSQL_DATABASE."'\n"); 
-echo "=> Found database '".MYSQL_DATABASE."'\n";
-
+$mysqli = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE) or die("Couldn't connect to MySQL on '".MYSQL_HOST."'' user '".MYSQL_USER."'\n");
+echo "\n=> Connected to Destination MySQL Server on ".MYSQL_HOST."\n";
 $mssql_tables = array();
-
 // Get MS SQL tables
 $sql = "SELECT * FROM sys.Tables;";
-$res = mssql_query($sql);
+$res = sqlsrv_query($mssql_db, $sql);
 echo "\n=> Getting tables..\n";
-while ($row = mssql_fetch_assoc($res))
+while ($row = sqlsrv_fetch_array($res))
 {
 	array_push($mssql_tables, $row['name']);
 	//echo ($row['name'])."\n";
 }
 echo "==> Found ". number_format(count($mssql_tables),0,',','.') ." tables\n\n";
-
 // Get Table Structures
 if (!empty($mssql_tables))
 {
@@ -73,24 +50,19 @@ if (!empty($mssql_tables))
 	{
 		echo '====> '.$i.'. '.$table."\n";
 		echo "=====> Getting info table ".$table." from SQL Server\n";
-
 		$sql = "select * from information_schema.columns where table_name = '".$table."'";
-		$res = mssql_query($sql);
-
+		$res = sqlsrv_query($mssql_db, $sql);
 		if ($res) 
 		{
 			$mssql_tables[$table] = array();
-
 			$mysql = "DROP TABLE IF EXISTS `".$table."`";
-			mysql_query($mysql);
+			$mysqli->query($mysql);
 			$mysql = "CREATE TABLE `".$table."`";
 			$strctsql = $fields = array();
-
-			while ($row = mssql_fetch_assoc($res))
+			while ($row = sqlsrv_fetch_array($res))
 			{
 				//print_r($row); echo "\n";
 				array_push($mssql_tables[$table], $row);
-
 				switch ($row['DATA_TYPE']) {
 					case 'bit':
 					case 'tinyint':
@@ -113,7 +85,6 @@ if (!empty($mssql_tables))
 					case 'numeric':
 						$data_type = $row['DATA_TYPE'].(!empty($row['NUMERIC_PRECISION']) ? '('.$row['NUMERIC_PRECISION'].(!empty($row['NUMERIC_SCALE']) ? ','.$row['NUMERIC_SCALE'] : '').')' : '' );
 						break;
-
 					case 'date':
 					case 'datetime':
 					case 'timestamp':
@@ -124,7 +95,6 @@ if (!empty($mssql_tables))
 					case 'smalldatetime':
 						$data_type = 'datetime';
 						break;
-
 					case 'nchar':
 					case 'char':
 						$data_type = 'char'.(!empty($row['CHARACTER_MAXIMUM_LENGTH']) && $row['CHARACTER_MAXIMUM_LENGTH'] > 0 ? '('.$row['CHARACTER_MAXIMUM_LENGTH'].')' : '(255)' );
@@ -137,18 +107,15 @@ if (!empty($mssql_tables))
 					case 'text':
 						$data_type = 'text';
 						break;
-
 					case 'binary':
 					case 'varbinary':
 						$data_type = $data_type = $row['DATA_TYPE'];
 					case 'image':
 						$data_type = 'blob';
 						break;
-
 					case 'uniqueidentifier':
 						$data_type = 'char(36)';//'CHAR(36) NOT NULL';
 						break;
-
 					case 'cursor':
 					case 'hierarchyid':
 					case 'sql_variant':
@@ -158,7 +125,6 @@ if (!empty($mssql_tables))
 						$data_type = false;
 						break;
 				}
-
 				if (!empty($data_type))
 				{
 					$ssql = "`".$row['COLUMN_NAME']."` ".$data_type." ".($row['IS_NULLABLE'] == 'YES' ? 'NULL' : 'NOT NULL');
@@ -167,18 +133,16 @@ if (!empty($mssql_tables))
 				}
 				
 			}
-
-			$mysql .= "(".implode(',', $strctsql).");";
+			$mysql .= "(".implode(',', $strctsql).") DEFAULT CHARACTER SET = 'utf8';";
 			echo "======> Creating table ".$table." on MySQL... ";
-			$q = mysql_query($mysql);
+			$q = $mysqli->query($mysql);
 			echo (($q) ? 'Success':'Failed!'."\n".$mysql."\n")."\n";
 			
 			echo "=====> Getting data from table ".$table." on SQL Server\n";
 			$sql = "SELECT * FROM ".$table;
-			$qres = mssql_query($sql);
-			$numrow = mssql_num_rows($qres);
+			$qres = sqlsrv_query($mssql_db, $sql, [], ['Scrollable' => 'static']);
+			$numrow = sqlsrv_num_rows($qres);
 			echo "======> Found ".number_format($numrow,0,',','.')." rows\n";
-
 			if ($qres)
 			{
 				echo "=====> Inserting to table ".$table." on MySQL\n";
@@ -186,15 +150,17 @@ if (!empty($mssql_tables))
 				if (!empty($fields))
 				{
 					$sfield = array_map('addTilde', $fields);
-					while ($qrow = mssql_fetch_assoc($qres))
+					while ($qrow = sqlsrv_fetch_array($qres))
 					{
 						$datas = array();
 						foreach ($fields as $field) 
 						{
 							$ddata = (!empty($qrow[$field])) ? $qrow[$field] : '';
-							array_push($datas,"'".mysql_real_escape_string($ddata)."'");
+							if ($ddata instanceof DateTimeInterface) {
+								$ddata = $ddata->format('c');
+							}
+							array_push($datas,"'".$mysqli->escape_string(utf8_decode($ddata))."'");
 						}
-
 						if (!empty($datas))
 						{
 							//$datas = array_map('addQuote', $datas);
@@ -202,23 +168,17 @@ if (!empty($mssql_tables))
 							$mysql = "INSERT INTO `".$table."` (".implode(',',$sfield).") VALUES (".implode(',',$datas).");";
 							//$mysql = mysql_real_escape_string($mysql);
 							//echo $mysql."\n";
-							$q = mysql_query($mysql);
+							$q = $mysqli->query($mysql);
 							$numdata += ($q ? 1 : 0 );
-						}
-						if ($numData % CHUNK_SIZE == 0) {
-							echo "===> ".number_format($numdata,0,',','.')." data inserted so far\n";
 						}
 					}
 				}
-				echo "======> ".number_format($numdata,0,',','.')." data inserted total\n\n";
+				echo "======> ".number_format($numdata,0,',','.')." data inserted\n\n";
 			}
 		}
 		$i++;
 	}
-
 }
-
 echo "Done!\n";
-
-mssql_close($mssql_connect);
-mysql_close($mysql_connect);
+sqlsrv_close($mssql_db);
+$mysqli->close();
